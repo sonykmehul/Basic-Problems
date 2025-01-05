5554b9083cb72676b60ccf3fcbf4159b80d24218
Mehul K Sony <sonykmehul@gmail.com>
2025-01-05 12:43:03 +0530

    Optimized Bubble Sort

---
+++ bubbleSort.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class bubbleSort {
+    public static void bubSort(int[] arr, int num){
+        int temp;
+        boolean swapped = false;
+        for(int i = 1; i <= num; i++){
+            for(int j = 0 ; j < num-1 ; j++){
+                if(arr[j] > arr[j+1]){
+                    swapped = true;
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                } 
+            }
+            if(swapped == false){
+                break;
+            }
+            
+            System.out.printf("Round %d: ", i);
+            System.out.println(Arrays.toString(arr));
+            System.out.println();
+        }
+
+        System.out.print("Sorted array (Bubble Sort): ");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        try (Scanner reader = new Scanner(System.in)) {
+            System.out.print("Enter the size of array: ");
+            int num = reader.nextInt();
+            int arr[];
+            arr = new int[num];
+            System.out.print("Enter the array: ");
+            for (int i = 0; i < num ; i++){
+                arr[i] = reader.nextInt();
+            }
+            bubSort(arr, num);
+        }
+    }
+}
